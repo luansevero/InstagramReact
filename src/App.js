16dd6604298508aae202763e039363573bacdff1
Luan Severo <luansevero@icloud.com>
2022-05-08 13:12:47 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./Navbar";
-
+import MobileFooter from "./Footer";
 export default function Instagram(){
     return(
         <div>
             <Navbar />
+            <MobileFooter />
         </div>
     )
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import Icons from "./Global/Icons";
+export default function MobileFooter(){
+    return(
+        <div class="fundo-mobile">
+            <Icons name="home" />
+            <Icons name="search-outline" />
+            <Icons name="add-circle-outline" />
+            <Icons name="heart-outline" />
+            <Icons name="person-outline" />
+        </div>
+    )
+}
